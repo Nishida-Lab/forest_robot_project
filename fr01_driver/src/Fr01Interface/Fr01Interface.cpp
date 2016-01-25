0eaaf692e8a6cf1453a2bbebe4622c62308ddbd7
Arita Yuta <k104009y@mail.kyutech.jp>
2016-01-26 00:12:10 +0900

    modified calucuration method of wheel speed

--- fr01_driver/src/Fr01Interface/Fr01Interface.cpp
+++ fr01_driver/src/Fr01Interface/Fr01Interface.cpp
@@ -40,44 +40,34 @@ void Fr01Interface::drive(double linear_speed, double angular_speed,
 {
   if(!isPivotTurn)
   {
-    if(angular_speed >= 0)
-    {
-      // steering
-      steer_input.position[0] = -atan2(2.0*tred_length_[0]*tan(angular_speed),
-                                       2.0*tred_length_[0]+2.0*tred_width_[0]*tan(angular_speed));
-      steer_input.position[1] = -atan2(2.0*tred_length_[1]*tan(angular_speed),
-                                       2.0*tred_length_[1]+2.0*tred_width_[0]*tan(angular_speed));
-      steer_input.position[2] = atan2(2.0*tred_length_[2]*tan(angular_speed),
-                                      2.0*tred_length_[2]+2.0*tred_width_[2]*tan(angular_speed));
-      steer_input.position[3] = atan2(2.0*tred_length_[3]*tan(angular_speed),
-                                      2.0*tred_length_[3]-2.0*tred_width_[2]*tan(angular_speed));
-      
-      // wheel
-      wheel_input.velocity[0] = linear_speed + (tred_width_[0])*angular_speed;
-      wheel_input.velocity[1] = linear_speed - (tred_width_[0])*angular_speed;
-      wheel_input.velocity[2] = linear_speed + (tred_width_[1])*angular_speed;
-      wheel_input.velocity[3] = linear_speed - (tred_width_[1])*angular_speed;
-      wheel_input.velocity[4] = linear_speed + (tred_width_[2])*angular_speed;
-      wheel_input.velocity[5] = linear_speed - (tred_width_[2])*angular_speed;
-    }
-    else
-    {
-      steer_input.position[0] = -atan2(2.0*tred_length_[0]*tan(angular_speed),
-                                       2.0*tred_length_[0]-2.0*tred_width_[0]*tan(angular_speed));
-      steer_input.position[1] = -atan2(2.0*tred_length_[1]*tan(angular_speed),
-                                       2.0*tred_length_[1]+2.0*tred_width_[0]*tan(angular_speed));
-      steer_input.position[2] = atan2(2.0*tred_length_[2]*tan(angular_speed),
-                                      2.0*tred_length_[2]-2.0*tred_width_[2]*tan(angular_speed));
-      steer_input.position[3] = atan2(2.0*tred_length_[3]*tan(angular_speed),
-                                      2.0*tred_length_[3]+2.0*tred_width_[2]*tan(angular_speed));
+    angular_speed = -angular_speed;
+    steer_input.position[2] = atan2(2.0*tred_length_[2]*tan(angular_speed),
+                                    2.0*tred_length_[2]+2.0*tred_width_[2]*tan(angular_speed));
+    steer_input.position[3] = atan2(2.0*tred_length_[3]*tan(angular_speed),
+                                    2.0*tred_length_[3]-2.0*tred_width_[2]*tan(angular_speed));
 
-      wheel_input.velocity[0] = linear_speed - (tred_width_[0])*angular_speed;
-      wheel_input.velocity[1] = linear_speed + (tred_width_[0])*angular_speed;
-      wheel_input.velocity[2] = linear_speed - (tred_width_[1])*angular_speed;
-      wheel_input.velocity[3] = linear_speed + (tred_width_[1])*angular_speed;
-      wheel_input.velocity[4] = linear_speed - (tred_width_[2])*angular_speed;
-      wheel_input.velocity[5] = linear_speed + (tred_width_[2])*angular_speed;
+    steer_input.position[0] = -steer_input.position[2];
+    steer_input.position[1] = -steer_input.position[3];
+      
+    // wheel
+    // 0 : left rear,   1 : right rear, 
+    // 2 : left middle, 3 : right middle, 
+    // 4 : left front,  5 : right front
+    if(angular_speed == 0.0)
+    {
+      for (size_t i = 0; i < 6; ++i) {
+        wheel_input.velocity[i] = linear_speed;
+      }
+    }else{
+      wheel_input.velocity[4] = (sin(angular_speed)/sin(steer_input.position[2]))*linear_speed;
+      wheel_input.velocity[0] = wheel_input.velocity[4];
+      wheel_input.velocity[5] = (sin(angular_speed)/sin(steer_input.position[3]))*linear_speed;
+      wheel_input.velocity[1] = wheel_input.velocity[5];
+    
+      wheel_input.velocity[2] = (sin(angular_speed)/tan(steer_input.position[2]))*linear_speed;
+      wheel_input.velocity[3] = (sin(angular_speed)/tan(steer_input.position[3]))*linear_speed;
     }
+    
   }else // pivot turn
   {
     //
